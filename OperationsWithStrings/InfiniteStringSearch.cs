6c034e3faab5f4516ab4d5834e927febfff044e7
Anastasiia <nastena31011993@gmail.com>
2019-06-24 23:33:45 +0300

    Add project files.

---
+++ OperationsWithStrings/InfiniteStringSearch.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Text;
+
+namespace OperationsWithStrings
+{
+    public static class InfiniteStringSearch
+    {
+        /* == Infinite string search ==
+         *
+         * Given a string "s" that defines a pattern for a infinite string, and a number "n" that
+         * defines the length of a substring that we should consider, you should create a function
+         * that counts the number of letters 'a' on it.
+         *
+         * Example 1:
+         * if the string is s = "abcac" and n = 10, the substring to be consider would be "abcacabcac".
+         * There are 4 occurrences of letter 'a' in this substring.
+         *
+         * Example 2:
+         * if the string is s = "aba" and n = 10, the substring to be consider would be "abaabaabaa".
+         * There are 7 occurrences of letter 'a' in this substring. 
+         *
+         * Parameters:
+         * - s: a string made with only lowercase letters
+         * - n: the desired size of the substring
+         *
+         * Constraints:
+         * - 1 <= s <= 100
+         * - 1 <= n <= 10^12
+         *
+         * Returns:
+         * <long> count of letters 'a' on the generated string
+         */
+        public static long GetNumberOfEntries(string pattern, int size, char character)
+        {
+            if (pattern.Length < 1 || pattern.Length > 100)
+            {
+                throw new ArgumentException("Length of string pattern should be between 1 and 100 charachers");
+            }
+
+            if (size < 1 || size > Math.Pow(10, 12))
+            {
+                throw new ArgumentException("Desired size of the substring should be a number between 1 and 10^12");
+            }
+
+            // for vizualization
+            var fullString = GetFullString(pattern, size);
+            Console.WriteLine($"Full string: {fullString}");
+
+            long count = 0;
+
+            var countOfEntriesInPattern = 0;
+
+            foreach (var item in pattern)
+            {
+                if (item == character)
+                {
+                    countOfEntriesInPattern++;
+                }
+            }
+            
+            long countOfPatternsInSize = size / pattern.Length;
+            long remainingStringLength = size % pattern.Length;
+
+            count = countOfPatternsInSize * countOfEntriesInPattern;
+
+            StringBuilder sb = new StringBuilder(pattern);
+            sb.Remove((int)remainingStringLength, pattern.Length - (int)remainingStringLength);
+
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (sb[i] == character)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        private static string GetFullString(string pattern, int size)
+        {
+            StringBuilder s = new StringBuilder();
+            while (s.Length < size)
+            {
+                s.Append(pattern);
+            }
+
+            if (s.Length > size)
+            {
+                s.Remove(size, s.Length - size);
+            }
+
+            return s.ToString();
+        }
+    }
+}
---
+++ OperationsWithStrings/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace OperationsWithStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get the second greatest element out of an array 
+            int[] arr = { 12, 35, 1, 10, 34, 1 };
+
+            var max = -1;
+
+            //Array.Sort(arr);
+            //Array.Reverse(arr);
+            //max = arr[1];
+
+            //for (int i = 0; i < arr.Length - 1; i++)
+            //{
+            //    if (max < arr[i])
+            //    {
+            //        max = arr[i];
+            //    }               
+            //}
+            //var max2 = -1;
+            //for (int i = 0; i < arr.Length - 1; i++)
+            //{
+            //    if (max2 < arr[i] && arr[i] < max)
+            //    {
+            //        max2 = arr[i];
+            //    }
+            //}
+
+            var secondMax = -1;
+            max = secondMax = arr[0];
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > max)
+                {
+                    secondMax = max;
+                    max = arr[i];
+                }
+                else if (arr[i] > secondMax)
+                {
+                    secondMax = arr[i];
+                }
+            }
+            Console.WriteLine(secondMax);
+            Console.WriteLine("--------------------------------------------");
+
+            // Fill a collection with random numbers from n to m without repetitions
+            var n = 6;
+            var m = 15;
+            var size = m - n;
+
+            var list = new HashSet<int>();
+            while (list.Count < size)
+            {
+                list.Add(GetRandomNumber(n, m));
+            }
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------------------------------------");
+            // Implement a method that accepts a sorted collection and an element which entries are to be found
+            Console.WriteLine("реализовать метод, который принимает в качестве параметров – " +
+                "коллекцию отсортированных целочисленных значений и элемент, который необходимо найти," +
+                " метод должен вернуть количество вхождений данного элемента ");
+
+            int[] arr2 = { 1, 2, 3, 4, 5, 5, 5, 5, 5, 6, 7, 8, 9 };
+
+            Console.WriteLine(GetNumberOfEntries(arr2, 5));
+            Console.WriteLine("----------------------------------------------");
+
+            // Infinite string search
+            Console.WriteLine(InfiniteStringSearch.GetNumberOfEntries("abcaadefg", 15, 'a'));
+            Console.ReadKey();
+        }
+        
+        public static int GetNumberOfEntries(int[] collection, int element)
+        {
+            var count = 0;
+
+            for (int i = 0; i < collection.Length - 1; i++)
+            {
+                if (collection[i] == element)
+                {
+                    count++;
+                    for (int j = i + 1; j < collection.Length - 1; j++)
+                    {
+                        if (collection[j] == element)
+                        {
+                            count++;
+                        }
+                        else
+                        {
+                            return count;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+        public static int GetRandomNumber(int n, int m)
+        {
+            Random rnd = new Random();
+            return rnd.Next(n, m);
+        }   
+    }
+}
