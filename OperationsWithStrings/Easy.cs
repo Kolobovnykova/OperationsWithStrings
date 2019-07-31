4d4aff85e8d96dc6e242413ea3e510470b1e9411
Anastasiia <nastena31011993@gmail.com>
2019-07-31 18:56:06 +0300

    Add 202 Happy Number

--- OperationsWithStrings/Easy.cs
+++ OperationsWithStrings/Easy.cs
@@ -471,5 +471,36 @@ namespace OperationsWithStrings
 
             return false;
         }
+
+        // 202. Happy Number
+        // Write an algorithm to determine if a number is "happy".
+        // A happy number is a number defined by the following process: Starting with any positive integer,
+        // replace the number by the sum of the squares of its digits, and repeat the process
+        // until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1.
+        // Those numbers for which this process ends in 1 are happy numbers.
+        public static bool IsHappy(int n)
+        {
+            var count = 0;
+            var k = 0;
+
+            while (count < 99)
+            {
+                while (n > 0)
+                {
+                    k = k + (int)Math.Pow(n % 10, 2);
+                    n = n / 10;
+                }
+
+                if (k == 1)
+                {
+                    return true;
+                }
+                n = k;
+                k = 0;
+                count++;
+            }
+
+            return false;
+        }
     }
 }
