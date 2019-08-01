b91bcc96fa1f91c049fce70034eb8c8763b93cee
Anastasiia <nastena31011993@gmail.com>
2019-08-01 16:56:41 +0300

    Add 88 Merged Sorted Array

--- OperationsWithStrings/Easy.cs
+++ OperationsWithStrings/Easy.cs
@@ -502,5 +502,43 @@ namespace OperationsWithStrings
 
             return false;
         }
+
+        // 88. Merge Sorted Array
+        // Given two sorted integer arrays nums1 and nums2, merge nums2 into nums1 as one sorted array.
+        // The number of elements initialized in nums1 and nums2 are m and n respectively.
+        // You may assume that nums1 has enough space (size that is greater or equal to m + n) to hold additional elements from nums2.
+        public static void MergeSortedArrays(int[] nums1, int n, int[] nums2, int m)
+        {
+            int k = n - 1;
+            int p = m - 1;
+            for (int i = n + m - 1; i >= 0; i--)
+            {
+                if (p < 0)
+                {
+                    nums1[i] = nums1[k];
+                    k--;
+                }
+                else if (k < 0)
+                {
+                    nums1[i] = nums2[p];
+                    p--;
+                }
+
+                if (p >= 0 && k >= 0)
+                {
+                    if (nums1[k] >= nums2[p])
+                    {
+                        nums1[i] = nums1[k];
+                        k--;
+                    }
+                    else
+                    {
+                        nums1[i] = nums2[p];
+                        p--;
+                    }
+                }
+
+            }
+        }
     }
 }
